d024fd8d802ebee71bc91a9873c496374860b110
samuel1333 <93929797+samuel1333@users.noreply.github.com>
2022-01-31 16:55:13 -0300

    treinando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import HelloWorld from './components/HelloWorld'
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
 
 function App() {
 
   return (
     <div className="App">
       <HelloWorld />
+      <SayMyName nome="bruh"/>
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props) {
+    return(
+    <div>
+        <h2>Payday {props.nome} kkkk</h2>
+    </div>
+    )
+}
+
+export default SayMyName
